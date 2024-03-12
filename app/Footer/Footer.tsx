53ac907bd426d724c8456942532669143a79d7e9
Дмитрий <Dmitriifrs@gmail.com>
2024-03-12 15:29:58 +0500

    homepage

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -1,4 +1,83 @@
+import styles from "./Footer.module.scss";
+import logo from "../../public/icons/MideaLogo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { FiPhone } from "react-icons/fi";
+import { AiTwotoneMail } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+import { CiInstagram } from "react-icons/ci";
+import { FaTelegramPlane } from "react-icons/fa";
+
+const nav = [
+   {
+      name: "О компании",
+      id: 0,
+   },
+   {
+      name: "Каталог продукции",
+      id: 1,
+   },
+   {
+      name: "Выполненные проекты",
+      id: 2,
+   },
+   {
+      name: "Контакты",
+      id: 3,
+   },
+   {
+      name: "Медиафайлы",
+      id: 4,
+   },
+];
+
 function Footer() {
-   return <footer className="footer">Footer</footer>;
+   return (
+      <footer className={`footer ${styles.footer}`}>
+         <div className={`${styles.footer__container} container`}>
+            <Link className={styles.footer__imgBody} href={"#"}>
+               <Image src={logo} alt="midea" fill />
+            </Link>
+            <nav className={styles.footer__nav}>
+               <h4 className={styles.footer__navTitle}>Навигация</h4>
+               <ul className={styles.footer__navList}>
+                  {nav.map((el) => {
+                     return (
+                        <Link style={{ color: "inherit" }} key={el.id} href={"#"}>
+                           <li className={styles.footer__navLink}>{el.name}</li>
+                        </Link>
+                     );
+                  })}
+               </ul>
+            </nav>
+            <div className={styles.footer__contacts}>
+               <h4 className={styles.footer__navTitle}>Контакты</h4>
+               <div className={styles.footer__navBody}>
+                  <FiPhone size={20} />
+                  <Link href={"#"} style={{ color: "inherit" }} className={styles.footer__phone}>
+                     +998(97) 333-63-57{" "}
+                  </Link>
+               </div>
+               <div className={styles.footer__navBody}>
+                  <AiTwotoneMail size={20} />
+                  <Link href={"#"} style={{ color: "inherit" }} className={styles.footer__email}>
+                     inbox@midea-asia.uz
+                  </Link>
+               </div>
+               <div className={styles.footer__iconsBody}>
+                  <Link className={styles.footer__icon} href={"#"}>
+                     <FaFacebookF color="#888888" size={35} />
+                  </Link>
+                  <Link className={styles.footer__icon} href={"#"}>
+                     <CiInstagram color="#888888" size={35} />
+                  </Link>
+                  <Link className={styles.footer__icon} href={"#"}>
+                     <FaTelegramPlane color="#888888" size={35} />
+                  </Link>
+               </div>
+            </div>
+         </div>
+      </footer>
+   );
 }
 export default Footer;
--- app/Homepage/Portfolio.tsx
+++ app/Homepage/Portfolio.tsx
@@ -1,11 +1,69 @@
+"use client";
+
 import styles from "./Main.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import SlideContent from "./SlideContent";
+import { Navigation } from "swiper/modules";
+import "./Swiper.css";
+import { MdNavigateNext } from "react-icons/md";
+import slideImg1 from "../../public/img/slide1.jpg";
+import slideImg2 from "../../public/img/slide2.jpg";
+import slideImg3 from "../../public/img/slide3.jpg";
+
+const sliderData = [
+   {
+      img: slideImg2,
+      title: "2014 FIFA World Cup Brazil Beira Rio Stadium",
+      subtitle: "It was the first time for Midea to enter the World Cup.",
+   },
+   {
+      img: slideImg1,
+      title: "2016 Rio Olympic Games Stadiums",
+      subtitle: "Midea provided HVAC for all the new-constructed stadiums",
+   },
+   {
+      img: slideImg3,
+      title: "2014 FIFA World Cup Brazil Beira Rio Stadium",
+      subtitle: "It was the first time for Midea to enter the World Cup.",
+   },
+   {
+      img: slideImg2,
+      title: "2014 FIFA World Cup Brazil Beira Rio Stadium",
+      subtitle: "It was the first time for Midea to enter the World Cup.",
+   },
+];
 
 function Portfolio() {
    return (
-      <section className={styles.portfolio}>
+      <section className={`${styles.portfolio} swip`}>
          <div className={styles.portfolio__bg}></div>
-         <h2 className={styles.portfolio__title}>ПОРТФОЛИО РАБОТ</h2>
-         <div className={styles.portfolio__slider}>Slider</div>
+         <h2 className={styles.portfolio__title}>Портфолио работ</h2>
+         <Swiper
+            className={styles.portfolio__swiper}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation={{
+               nextEl: ".swiper-button-next",
+               prevEl: ".swiper-button-prev",
+            }}
+            modules={[Navigation]}
+            grabCursor
+         >
+            <button className="swiper-button-next">
+               <MdNavigateNext />
+            </button>
+            <button className="swiper-button-prev">
+               <MdNavigateNext />
+            </button>
+            {sliderData.map((el, index) => {
+               return (
+                  <SwiperSlide key={index}>
+                     <SlideContent img={el.img} title={el.title} subtitle={el.subtitle} />
+                  </SwiperSlide>
+               );
+            })}
+         </Swiper>
       </section>
    );
 }
